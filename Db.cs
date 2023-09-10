d8a1d9293de9c61452cddef51300eb62713a1f78
my9845115777 <m9845115777@gmail.com>
2023-09-10 23:58:25 +0530

    camera-1

---
+++ Db.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+namespace Monitoring
+{
+    class Db
+    {
+        MySqlConnection con = null;
+        MySqlCommand cmd = null;
+
+        public DataTable getHelpRecords()
+        {
+            con = new MySqlConnection("server=sg2nlmysql35plsk.secureserver.net;port=3306;database=samplemydb;user id=usrsamplemydb;pwd=c99&4pXk");
+            con.Open();
+            string sql = "select users.name , users.mobile , locations.latitude , locations.longitude , locations.created_at , locations.id from users,locations where users.id = locations.user_id and status = 'N'";
+            MySqlDataAdapter adap = new MySqlDataAdapter(sql, con);
+            DataTable buffer = new DataTable();
+            adap.Fill(buffer);
+            con.Close();
+            return buffer;
+        }
+
+        public void Update(string id)
+        {
+            con = new MySqlConnection("server=sg2nlmysql35plsk.secureserver.net;port=3306;database=samplemydb;user id=usrsamplemydb;pwd=c99&4pXk");
+            con.Open();
+            string sql = "update locations set status = 'Y' where id = " + id;
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+
+        public DataTable getAllHelpRecords()
+        {
+            con = new MySqlConnection("server=sg2nlmysql35plsk.secureserver.net;port=3306;database=samplemydb;user id=usrsamplemydb;pwd=c99&4pXk");
+            con.Open();
+            string sql = "select * from locations";
+            MySqlDataAdapter adap = new MySqlDataAdapter(sql, con);
+            DataTable buffer = new DataTable();
+            adap.Fill(buffer);
+            con.Close();
+            return buffer;
+        }
+
+    }
+}
---
+++ Earth.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Monitoring
+{
+        class LatLon
+        {
+                public double Latitude, Longitude;
+                public LatLon(double Lat, double Lon)
+                {
+                    Latitude = Lat;
+                    Longitude = Lon;
+                }
+        }
+
+    class Earth
+    {
+        public static LatLon CalculateDerivedPosition(LatLon source, double distance, double angle)
+        {
+            double distanceNorth = Math.Sin(angle) * distance;
+            double distanceEast = Math.Cos(angle) * distance;
+                
+            double earthRadius = 6371000;
+
+            double newLat = source.Latitude + (distanceNorth / earthRadius) * 180 / Math.PI;
+            double newLon = source.Longitude + (distanceEast / (earthRadius * Math.Cos(newLat * 180 / Math.PI))) * 180 / Math.PI;
+            return new LatLon(newLat, newLon);
+        }
+
+        public static double GetDistance(double lat1, double lon1, double lat2, double lon2, char unit)
+        {
+            double theta = lon1 - lon2;
+            double dist = Math.Sin(deg2rad(lat1)) * Math.Sin(deg2rad(lat2)) + Math.Cos(deg2rad(lat1)) * Math.Cos(deg2rad(lat2)) * Math.Cos(deg2rad(theta));
+            dist = Math.Acos(dist);
+            dist = rad2deg(dist);
+            dist = dist * 60 * 1.1515;
+            if (unit == 'K')
+            {
+                dist = dist * 1.609344;
+            }
+            else if (unit == 'N')
+            {
+                dist = dist * 0.8684;
+            }
+            return (dist);
+        }
+
+        private static double deg2rad(double deg)
+        {
+            return (deg * Math.PI / 180.0);
+        }
+        private static double rad2deg(double rad)
+        {
+            return (rad / Math.PI * 180.0);
+        }
+
+
+    }
+
+
+
+
+}
